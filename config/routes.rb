f7c96e5b1bd4cd1c18b682c357eb475b32fa9af9
watanabenao <watanabenao@watanabenoMacBook-puro.local>
2019-02-25 00:27:03 +0900

    Logout-page MarkUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root 'products#index'
- resources :users, only: [:index] do
+ resources :users, only: [:index,] do
    collection do
      get 'profile'
      get 'identification'
+     get 'logout'
    end
  end
 
