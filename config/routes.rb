873e6a58d42158361e3854b1f90130c0de331a61
watanabenao <watanabenao@watanabenoMacBook-puro.local>
2019-03-05 01:07:02 +0900

    User-signup Markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    sessions:'users/sessions',
+    registration: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#link'
+    get 'signup/registration', to: 'devise/registrations#new'
+  end
+
+
  root 'products#index'
  resources :users, only: [:index,] do
    collection do
