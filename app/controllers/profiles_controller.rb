7a1cbff013cd70e8ddbf561da9a7d9fd524da37f
watanabenao <watanabenao@watanabenoMacBook-puro.local>
2019-03-08 13:52:12 +0900

    住所、カード登録機能の実装　修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,5 @@
 class ProfilesController < ApplicationController
 
-  # def create
-  #   Profile.create(profile_params)
-  # end
-
-  # private
-  #  def profile_params
-  #    params.require(:profile).permit(:postal_code, :prefecture, :city, :address, :building, :sub_phone).merge(user_id: current_user.id)
-  #  end
 end
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = current_user
   end
 
   def update
